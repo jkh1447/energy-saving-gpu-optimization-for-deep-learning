9b908baa6cd6fe333f9edaa285c778d5b92c5c80
jkh1447 <jkh1447@gmail.com>
2024-05-12 03:04:26 +0900

    neural network

---
+++ ReNet18.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+# GPU 사용 가능 여부 확인
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 데이터 전처리
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# CIFAR-10 데이터셋 다운로드
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+
+# 모델 정의 (예시로 ResNet18 사용)
+resnet18 = models.resnet18()
+
+# 모델을 GPU로 이동
+resnet18 = resnet18.to(device)
+
+# 손실 함수와 옵티마이저 정의
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(resnet18.parameters(), lr=0.001, momentum=0.9)
+
+# 학습
+epochs = 5
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data[0].to(device), data[1].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = resnet18(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 1000 == 999:  # 1000 미니배치마다 손실 출력
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 1000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# 평가
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = resnet18(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
